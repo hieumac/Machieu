d56f15b6dd43efa0d588bba59b8a5f4f6a3c6b1d
MacHieu <macvanhieu27022001@gmail.com>
2022-04-18 22:21:35 +0700

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { isNull } from '@angular/compiler/src/output/output_ast';
+import { Injectable } from '@angular/core';
+import { empty, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(2);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User): void => {
+        const user = response;
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/user',model).pipe(
+      map((user: User) => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  };
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null!);
+  }
+}
